81d13db1dccb85d903f9bf296871f9169aac189c
Brydon Cheyney <brydon@remission.org.uk>
2017-06-12 21:52:33 +0100

    added :signing :gpg-key for clojars deploy

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject strict-config "0.1.0-SNAPSHOT"
+(defproject strict-config "0.1.0"
   :description "Less tolerant Clojure service configuration."
   :url "https://github.com/brydoncheyney/strict-config"
   :license {:name "MIT License"}
   :dependencies	[[org.clojure/clojure "1.5.1"]]
+  :signing {:gpg-key "3F43809AE125AE7217006EB0BED6059AE9D61E7C"}
   :profiles {:dev
              {:plugins [[lein-midje "3.2.1"] ]
               :dependencies [[midje "1.8.3"]]}})
